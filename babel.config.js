8eeec502c99b80395e6635507bf69c6a15768690
Daniel <danieuuul@gmail.com>
2022-05-05 14:36:58 -0300

    Fix babel

--- babel.config.js
+++ babel.config.js
@@ -8,11 +8,11 @@ module.exports = {
       "module-resolver",
       {
         alias: {
-          "@modules": ["./src/modules"],
-          "@config": ["./src/config"],
-          "@shared": ["./src/shared"],
-          "@error": ["./src/shared/errors"],
-          "@utils": ["./src/utils"],
+          "@modules": "./src/modules",
+          "@config": "./src/config",
+          "@shared": "./src/shared",
+          "@error": "./src/shared/errors",
+          "@utils": "./src/utils",
         },
       },
     ],
