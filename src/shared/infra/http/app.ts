f735a54bc461b28a15c33a48a30a800f5dba715b
Daniel <danieuuul@gmail.com>
2022-05-06 13:03:47 -0300

    Add library cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,13 +2,13 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import "reflect-metadata";
 import "dotenv/config";
+import cors from "cors";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import swaggerUI from "swagger-ui-express";
 
-import createConnection from "@shared/infra/typeorm";
-
-import "../../container";
 import upload from "@config/upload";
+import createConnection from "@shared/infra/typeorm";
+import "../../container";
 
 import AppError from "../../errors/AppError";
 import swaggerDoccument from "./swagger.json";
@@ -26,6 +26,7 @@ const app = express();
   app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
   app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+  app.use(cors());
   app.use(router);
 
   app.use(
